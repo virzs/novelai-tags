6532409f915b12205fb30aad37738a795e9d31a1
vir <virs98@outlook.com>
2022-11-15 23:21:33 +0800

    :bug: fix build error

--- website/lib/dbConnect.ts
+++ website/lib/dbConnect.ts
@@ -2,7 +2,7 @@
  * @Author: vir virs98@outlook.com
  * @Date: 2022-11-05 17:56:08
  * @LastEditors: vir virs98@outlook.com
- * @LastEditTime: 2022-11-07 16:05:20
+ * @LastEditTime: 2022-11-15 23:20:33
  */
 
 import mongoose from "mongoose";
@@ -10,10 +10,10 @@ import dbConfig from "../configs/dbConfig";
 
 const { host, port, database, username, password, dataPath } = dbConfig;
 
-let cached = global.mongoose;
+let cached = (global as any).mongoose;
 
 if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+  cached = (global as any).mongoose = { conn: null, promise: null };
 }
 
 const dbConnect = async () => {
